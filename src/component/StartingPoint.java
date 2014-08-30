51dd60bcccfbb203320b2fe9312974eaabb501c8
AayushB <aayush.bhandarii@gmail.com>
2014-08-30 12:15:05 -0700

    Clean up and added comments for main menu to game transition

--- src/component/StartingPoint.java
+++ src/component/StartingPoint.java
@@ -1,7 +1,6 @@
 package component;
 import java.applet.*;
 import java.awt.*;
-
 import javax.swing.UIManager;
 import javax.swing.UnsupportedLookAndFeelException;
 
@@ -18,7 +17,6 @@ public class StartingPoint extends Applet implements Runnable
 		@Override
 		public void init() 
 		{
-			
 			//Platform independence for swing
 		    try 
 		    {
@@ -35,7 +33,7 @@ public class StartingPoint extends Applet implements Runnable
 			appletWidth=1000;
 			appletHeight=616;
 			
-			//Applet setup and component adding
+			//Applet setup and add KeyBoard Listener
 			this.setSize(appletWidth, appletHeight); // width, height
 			this.setBackground(Color.BLACK); 
 			this.setFocusable(true);
@@ -46,6 +44,7 @@ public class StartingPoint extends Applet implements Runnable
 			mainMenuPanel = new MainMenuPanel(appletWidth, appletHeight);
 			storyPanel= new StoryPanel(appletWidth, appletHeight, kListener);
 			
+			//Start the thread
 			Thread thread= new Thread(this);
 			thread.start();
 		}
@@ -53,6 +52,7 @@ public class StartingPoint extends Applet implements Runnable
 		@Override
 		public void run() 
 		{
+			//We start the game with main menu
 			mainMenuPanel.start();
 			this.add(mainMenuPanel);
 			
@@ -61,18 +61,19 @@ public class StartingPoint extends Applet implements Runnable
 				//while in menu
 				if(mainMenuPanel.getPlayClicked())
 				{
-					 //System.out.println("Here");
+					//Remove main menu from the applet
 					mainMenuPanel.setPlayClicked(false);
 					mainMenuPanel.end();
 					this.remove(mainMenuPanel);
-					//mainMenuPanel=null;
-					this.setFocusable(true);
-					
-					
+					//Add Story Panel to the applet
 					storyPanel.start();
-					this.add(storyPanel);
-					this.validate();			
+					this.add(storyPanel);			
 				}
+				
+				//Validate to Paint the story Panel
+				this.validate();
+				
+				
 				//while in a different screen
 				try 
 				{
@@ -81,13 +82,6 @@ public class StartingPoint extends Applet implements Runnable
 				catch (InterruptedException e) {}
 			}
 				
-				
-				
-				
-				//
-				
-			}
-
-		
+		}
 }
 
